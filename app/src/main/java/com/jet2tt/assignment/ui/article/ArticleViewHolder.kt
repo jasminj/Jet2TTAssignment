937c181af2a5547b957ee43d4c412031c3408ea5
Jasmin <jasmin.javia@peaas.co>
2020-09-14 12:35:44 +0530

    Articles screen UI design and set data
    Articles screen UI design and set data

--- app/src/main/java/com/jet2tt/assignment/ui/article/ArticleViewHolder.kt
+++ app/src/main/java/com/jet2tt/assignment/ui/article/ArticleViewHolder.kt
@@ -1,6 +1,7 @@
 package com.jet2tt.assignment.ui.article
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -8,6 +9,7 @@ import com.bumptech.glide.Glide
 import com.jet2tt.assignment.R
 import com.jet2tt.assignment.databinding.ItemArticleBinding
 import com.jet2tt.assignment.model.Article
+import com.jet2tt.assignment.util.Utils
 
 class ArticleViewHolder(private var binding: ItemArticleBinding) :
     RecyclerView.ViewHolder(binding.root) {
@@ -23,10 +25,52 @@ class ArticleViewHolder(private var binding: ItemArticleBinding) :
 
     fun bind(article: Article) {
         this.article = article
+        val user = article.user[0]
+
         if (article.media.isNotEmpty()) {
-            Glide.with(binding.image)
-                .load(article.media[0].image)
-                .into(binding.image)
+            val media = article.media[0]
+
+            Glide.with(binding.articleImage)
+                .load(media.image)
+                .into(binding.articleImage)
+
+            binding.articleTitle.text = media.title
+            binding.articleUrl.text = media.url
+            showViews(binding.articleImage, binding.articleTitle, binding.articleUrl)
+        } else {
+            hideViews(binding.articleImage, binding.articleTitle, binding.articleUrl)
+        }
+
+        Glide.with(binding.userImage)
+            .load(user.avatar)
+            .into(binding.userImage)
+
+        binding.userName.text = user.name
+        binding.userDesignation.text = user.designation
+        binding.articleContent.text = article.content
+        binding.createdAt.text = Utils.getTimeAgo(article.createdAt)
+
+        binding.articleLikes.text =
+            binding.root.resources.getString(
+                R.string.likes,
+                Utils.abbrevCount(article.likes)
+            )
+        binding.articleComments.text =
+            binding.root.resources.getString(
+                R.string.comments,
+                Utils.abbrevCount(article.comments)
+            )
+    }
+
+    private fun showViews(vararg views: View) {
+        for (view in views) {
+            view.visibility = View.VISIBLE
+        }
+    }
+
+    private fun hideViews(vararg views: View) {
+        for (view in views) {
+            view.visibility = View.GONE
         }
     }
 
